63b8c3d61bfab7f4e4de9101965bc557949aaf3b
NovusEdge <novusedge0@gmail.com>
2026-09-05 15:49:00 +0300

    test(cli): pin bare vm resolution at project scope
    Signed-off-by: NovusEdge <novusedge0@gmail.com>

---
+++ internal/cli/project.go
@@ -0,0 +1,9 @@
+package cli
+
+import "errors"
+
+// resolveScope loads the project in the current directory, if any, and turns
+// a bare VM argument into a global name.
+func resolveScope(a *Args) error {
+	return errors.New("cli: not implemented")
+}
