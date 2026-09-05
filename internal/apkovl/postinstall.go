d5ee52d2cbc3436f8cfc3c69da9814ffe31eaab6
NovusEdge <novusedge0@gmail.com>
2026-09-05 03:38:00 +0300

    test(apkovl): pin the three boot-defect fixes
    Signed-off-by: NovusEdge <novusedge0@gmail.com>

---
+++ internal/apkovl/postinstall.go
@@ -0,0 +1,9 @@
+package apkovl
+
+// extlinuxTimeoutFix, workMountFix and issueFix are stubs. Tasks 8, 9 and 10
+// each replace one body with the sh fragment that closes its issue.
+const extlinuxTimeoutFix = ``
+
+const workMountFix = ``
+
+const issueFix = ``
