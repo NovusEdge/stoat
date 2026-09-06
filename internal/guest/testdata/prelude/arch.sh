7dab1c7057342f31b3bc4b2c7307f63d2ce7b0cb
Aliasgar Khimani <68768969+NovusEdge@users.noreply.github.com>
2026-09-06 21:37:37 +0300

    fix(guest): pass --needed to pacman (#105)
    devtools and build-deps both install base-devel on Arch. Without --needed,
    a VM that selects both downloads and reinstalls the whole group.
    
    Closes #103
    
    Signed-off-by: NovusEdge <novusedge0@gmail.com>

--- internal/guest/testdata/prelude/arch.sh
+++ internal/guest/testdata/prelude/arch.sh
@@ -1,5 +1,5 @@
 stoat_pkg_setup() { pacman -Sy --noconfirm; }
-stoat_pkg_install() { 'pacman' '-S' '--noconfirm' "$@"; }
+stoat_pkg_install() { 'pacman' '-S' '--noconfirm' '--needed' "$@"; }
 stoat_svc_enable() { systemctl enable "$1"; }
 stoat_svc_start() { systemctl start "$1"; }
 stoat_svc_stop() { systemctl stop "$1"; }
